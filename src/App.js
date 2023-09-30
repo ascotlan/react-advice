040f4a13cd73f003c53ab06788215b57aa6b8813
ascotlan <antonio.k.scotland@gmail.com>
2023-09-30 16:41:16 -0400

    Implement state to track changes to the DOM

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  async function getAdvice(){
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const data = await response.json();
-    console.log(data.slip);
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  const getAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setCount((currentCount) => currentCount + 1);
+      });
   };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message({ count }) {
+  return (
+    <p>
+      You have read <strong>{count}</strong> pieces of advice
+    </p>
   );
 }
